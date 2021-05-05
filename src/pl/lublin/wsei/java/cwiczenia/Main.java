3bf77230361a9706ce1d1a5bc0dfddca9edbde5d
Michał Czaja <35144@studentwsei.pl>
2021-05-05 12:07:46 +0200

    Dodanie komentarzy

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,7 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
+    /**
+     * comment before main()
+     */
     public static void main(String[] args) {
+        /*
+        multiple
+        line
+        comment
+        */
 
+        // single line comment
     }
 }
