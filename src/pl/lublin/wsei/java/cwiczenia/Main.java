a4c0b94a2006a8e3cfd5dc3774a4bdb4822921a3
Michał Czaja <35144@studentwsei.pl>
2021-05-05 17:30:22 +0200

    Zadania nr.8-9

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,52 +1,44 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Scanner;
+
 public class Main {
-    /**
-     * comment before main()
-     */
     public static void main(String[] args) {
-        /*
-        multiple
-        line
-        comment
-        */
+        Scanner input = new Scanner(System.in);
 
-        // single line comment
-        System.out.println("Hello Java world\n");
 
-        System.out.print("Ala");
-        System.out.print("ma");
-        System.out.print("kota");
+        int num1 = 0, num2 = 0;
+        do {
+            System.out.print("Podaj pierwszą liczbę: ");
+            num1 = input.nextInt();
+            if (num1 != 0)
+            {
+                System.out.print("Podaj drugą liczbę: ");
+                num2 = input.nextInt();
+            }
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+        } while ((num1 != 0)&&(num2 != 0));
 
-        System.out.println("Ala");
-        System.out.println("ma");
-        System.out.println("kota");
 
-        System.out.print("Ala\n");
-        System.out.print("ma\n");
-        System.out.print("kota\n");
+        int num3 = 0, num4 = 0;
+        do {
+            System.out.print("Podaj pierwszą liczbę: ");
+            num3 = input.nextInt();
+            if (num3 == 0) break;
+            System.out.print("Podaj drugą liczbę: ");
+            num4 = input.nextInt();
+            if (num4 == 0) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num3, num4, num3 + num4);
+        } while (true);
 
-        int a = 3;
-        double b = 4.21;
-        String s = "jakiś tekst";
 
-        System.out.printf("a = %d, b = %f, s = %s %n", a, b, s);
-
-        System.out.printf("a = %d, b = %.2f, s = %20s %n", a, b, s);
-
-        System.out.printf(" Nazywaliśmy to \"witaminą B3\"");
-
-        System.out.printf("alfa\tsin (alfa)\n");
-        for (int i = 0; i < 370; i+=10)
-        {
-            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
-        }
-
-        System.out.printf("arg\tlog2(arg)\t sum(arg)\n");
-        int sum = 0;
-        for (int i = 1; i <= 16; i = i*2)
-        {
-            System.out.printf("%d\t%f\t%d\n", i, Math.log(i)/Math.log(2), sum = sum+i);
-        }
+        int num5 = 0, num6 = 0;
+        do {
+            System.out.print("Podal liczby, które mam dodać: ");
+            num5 = input.nextInt();
+            num6 = input.nextInt();
+            if ((num5 == 0) || (num6 == 0)) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num5, num6, num5 + num6);
+        } while (true);
     }
 }
